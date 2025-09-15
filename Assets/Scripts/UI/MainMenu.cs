4bb02906899fde186359a730994a5defe57f202e
Nick Maltbie <nick.dmalt@gmail.com>
2025-09-14 22:16:29 -0700

    Added main menu and menus within each level to quit (#11)

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    public SceneField level1;
+
+    public void StartGame()
+    {
+        Transition.ToScene(level1.SceneName);
+    }
+}
---
+++ Assets/Scripts/UI/OpenMenu.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+class OpenMenu : MonoBehaviour
+{
+    public InputActionReference openMenu;
+    public GameObject menuObject;
+
+    public void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        openMenu.action.performed += this.OnOpenMenu;
+    }
+
+    public void OnDestroy()
+    {
+        openMenu.action.performed -= this.OnOpenMenu;
+    }
+
+    public void OnOpenMenu(InputAction.CallbackContext context)
+    {
+        ToggleMenu();
+    }
+
+    public void ToggleMenu()
+    {
+        // toggle menu object
+        menuObject.SetActive(!menuObject.activeInHierarchy);
+
+        if (menuObject.activeInHierarchy)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
